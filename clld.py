ff44cc22fc79e6c754c0f96ece08df904d853ddd
Brock Smedley <2791467+BrockSmedley@users.noreply.github.com>
2018-07-09 22:07:49 -0700

    bugfix delete, more docs & tests
    + modified .gitignore

--- clld.py
+++ clld.py
@@ -8,45 +8,61 @@
     ### example:
     ###         {123: 420, 420: 69, 82: 14, 69:82, 14:123}      
 class Clld:
-    # can init with array
+    ### Constructor. Optional init with root value.
     def __init__(self, root=None):
+        self.refresh(root)
+
+    ### clears list, optionally sets new root
+    def refresh(self, newVal=None):
         self.head = None
         self.tail = None
         self.clld = {}
-        
-        if (type(root) == int):
-            self.clld.update({root:root})
-            self.head = root
-            self.tail = root
+        if (type(newVal) == int):
+            self.head = newVal
+            self.tail = newVal
+            self.clld.update({newVal:newVal})
 
-        
+    ### add new value to list
     def add(self, val):
-        # set old tail:next (currently pointing at head)
-        self.clld[self.tail] = val
-        # add new val to dict; point at head for circularity
-        self.clld[val] = self.head
-        # store new tail for later
-        self.tail = val
+        if (self.tail == None or self.head == None):
+            # must be the first item; late initialization OK (:
+            self.refresh(val)
+        else:
+            # set old tail:next (currently pointing at head)
+            self.clld[self.tail] = val
+            # add new val to dict; point at head for circularity
+            self.clld[val] = self.head
+            # store new tail for later
+            self.tail = val
 
-        
+    ### delete a specific value from the list
     def delete(self, val):
         # transplant links from val item
-        p = prevItem(val)
-        n = nextItem(val)
+        p = self.prevItem(val)
+        n = self.nextItem(val)
+        if (val == self.tail): # if we delete tail, update it
+            self.tail = p
+        elif (val == self.head): # if we delete head, update it
+            self.head = n
         self.clld[p] = n
 
         # delete val from dict
         del self.clld[val]
-        
 
+        
+    ### returns item "pointed to" by val
     def nextItem(self, val):
         # simply return key's value from dict
         return self.clld[val]
 
-
+    ### returns item "pointing to" val
     def prevItem(self, val):
         # loops all the way through the list back to the item pointing at val
         n = self.head
         while (self.clld[n] != val):
             n = self.clld[n]
         return n
+
+    ### returns length of list
+    def length(self):
+        return len(self.clld.keys())
--- test.py
+++ test.py
@@ -1,15 +1,58 @@
 from clld import Clld
 
-def test():
-    c = Clld(1)
-    c.add(3)
-    c.add(42)
+def testLoop(c):
+    print "Looping through list 1.5 times..."
     x = c.head
     print x
-    for i in range (5):
+    for i in range (c.length() + c.length()/2):
         x = c.nextItem(x)
         print x
         if (x == c.tail):
             print "end of list; looping back"
+    print "Done for now.\n"
+
+# deletes a value at a given distance from head: nn
+def testDelete(c, nn):
+    nn = nn%c.length()
+    x = c.head
+    for i in range (nn):
+        x = c.nextItem(x)
+    c.delete(x)
+    print "Deleted item #%d..." % (nn)
+
+def testAdd(c, n):
+    c.add(n)
+    print "Added an item: %d..." % (n)
+
+def testPrev(c, n):
+    print "The number before %d is %d." % (n, c.prevItem(n))
+
+def testInit(n=None):
+    c = Clld(n)
+    if (n != None):
+        extra = " with value %s" % (n)
+    else:
+        extra = ""
+    print "Initialized new clld%s." % (extra)
+    return c
+
+def sanityCheck(c):
+    print "Full dict: %s" % (str(c.clld))
+    print "Head: %d" % (c.head)
+    print "Tail: %d" % (c.tail)
+
+def test():
+    c = testInit(1)
+    testAdd(c, 3)
+    testAdd(c, 42)
+    testLoop(c)
+    testPrev(c, 1)
+    testPrev(c, 42)    
+    testDelete(c, 2)
+    testLoop(c)
+    m = [5,7,9]
+    for i in m:
+        testAdd(c, i)
+    testLoop(c)
 
 test()
